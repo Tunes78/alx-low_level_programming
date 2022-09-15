30f3207771b89c3c030ec9cd8371047c50863a7b
Fortune Nnadi <pafort49@gmail.com>
2022-09-14 18:57:47 -0700

    this is the main.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* add - adds two integers*
+* @n1: integer 1
+* @n2: integer 2
+* Description: give two integers, returns sum
+* Return: sum
+*/
+
+void print_alphabet(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 10; i++ )
+	{
+		for (c='a'; c<='z'; ++c)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
+	return 0;
+
+}
